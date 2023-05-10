0fb6b8acddd4a7ba069f8b84a4e843aab40d8890
Bryan M <bmaitland101@gmail.com>
2023-05-10 11:58:56 -0500

    Clean up for final release

--- R/00_prep.R
+++ R/00_prep.R
@@ -1,20 +1,36 @@
 
+# Prepare work space for analyses
 
-# Global libraries
+# Load libraries
 pacman::p_load(
-  tidyverse, here, cowplot, mgcv, gratia, vegan, funrar, broom, rKIN, lme4, lmerTest)
+  tidyverse, 
+  here, 
+  cowplot, 
+  mgcv, 
+  gratia, 
+  vegan, 
+  funrar, 
+  broom, 
+  rKIN, 
+  lme4, 
+  lmerTest
+  )
 
-# PLotting theme
+# Load custom plotting theme
 source(here("R", "fx_theme_pub.R"))
+
+# Set custom theme as default
 theme_set(theme_Publication())
 
-
-# stream labels
+# Create labels for all stream systems
 label_streams <- c("Laramie","MedBow","Sweetwater","Horse")
 
-# site id labels
+# Create site id labels
 label_sites_ids <- 
-  c("LR00","LR01","LR02","LR03","LR04","LR05","LR06","LR07","LR08",
+  c(
+    "LR00","LR01","LR02","LR03","LR04","LR05","LR06","LR07","LR08",
     "MB01","MB02","MB03","MB04",
     "SW01","SW02","SW03","SW04",
-    "HC00","HC01","HC02")
+    "HC00","HC01","HC02"
+    )
+
